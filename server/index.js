d1053c79ac5622e5c6ca6fb04075a36c4516c4b7
undg <und3rdg@gmail.com>
2024-10-10 19:49:46 +0100

    endpoint

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 const cors = require("cors");
+const {calculateScore} = require("./calculateScore")
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(pino);
 
 app.get('/api/greeting', (req, res) => {
@@ -16,3 +18,10 @@ app.get('/api/greeting', (req, res) => {
 app.listen(3001, () =>
   console.log('Express server is running on localhost:3001')
 );
+
+app.post('/api/scrabble-score', (req, res) => {
+  const { word } = req.body;
+  res.json({ score: calculateScore(word) });
+});
+
+
