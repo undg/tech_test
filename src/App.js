ab3ca7547a6ce893a10a1064852ccc44f6e941ad
undg <und3rdg@gmail.com>
2024-10-10 20:51:26 +0100

    unstyled input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
+import dict from "./dict";
 
-class App extends Component {
-  render() {
-    return <div className="App">hello world</div>;
-  }
+function App() {
+
+  const [word, setWord] = React.useState("");
+  const handleSubmit = () => {}
+  return (
+    <div className="App" data-testid={dict.testid.app}>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={word}
+          onChange={(e) => setWord(e.target.value)}
+          placeholder="Enter word"
+        />
+        <button type="submit">Calculate Score</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
