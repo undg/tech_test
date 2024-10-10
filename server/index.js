25f13632011b6f8f041ab8dacb944e42a3a19099
undg <und3rdg@gmail.com>
2024-10-10 20:24:20 +0100

    wrong import. Unit testin LoL https://www.monkeyuser.com/2022/unit-tests/ 🤣🤣🤣

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
+const express = require("express");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
 const cors = require("cors");
-const {calculateScore} = require("./calculateScore")
+const { calculateScore } = require("./calculate-score");
 
 const app = express();
 app.use(cors());
@@ -10,19 +10,17 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
 
-app.get('/api/greeting', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+app.get("/api/greeting", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ greeting: `Hello World!` }));
 });
 
 app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+  console.log("Express server is running on localhost:3001"),
 );
 
-app.post('/api/scrabble-score', (req, res) => {
+app.post("/api/scrabble-score", (req, res) => {
   const { word } = req.body;
-  const {score, err} = calculateScore(word)
+  const { score, err } = calculateScore(word);
   res.json({ score, err });
 });
-
-
